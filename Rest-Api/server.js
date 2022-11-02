8cfedbf68ec9f1fbdc8b5107f846b3b7d0aa3666
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-02 14:21:45 +0200

    add router

--- Rest-Api/server.js
+++ Rest-Api/server.js
@@ -2,20 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const { initDB } = require("./config/database");
-const expressConfig = require("./config/express");
+const router = require("./router");
 
 dotenv.config({
   path: __dirname + "/config.env",
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 async function startServer() {
   const app = express();
 
   await initDB();
-
+  
   app.use(express.json());
+
+  app.use(router);
 
   app.listen(port, () => console.log(`Server listening on port ${port}...`));
 }
