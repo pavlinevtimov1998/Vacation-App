a87051cb10d102f033cc121389176fc55d91e26a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-02 14:39:51 +0200

    add basic user model

---
+++ Rest-Api/Models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const salt = process.env.SALT;
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    // required: [true, "Username is required!"],
+    // minLength: [2, "Username should be at least 2 characters!"],
+  },
+  email: {
+    type: String,
+    // required: [true, "Email is required!"],
+    // minLength: [10, "Email should be at least 10 characters!"],
+  },
+  password: {
+    type: String,
+    // minLength: [4, "Password should be at least 4 characters!"],
+    // required: [true, "Password is required!"],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+
+  this.password = hashedPassword;
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
