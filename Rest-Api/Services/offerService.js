28fee346f5af7c650625285df9bce49f032bbcb1
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-03 13:51:27 +0200

    Update offerService.js

--- Rest-Api/Services/offerService.js
+++ Rest-Api/Services/offerService.js
@@ -1,17 +1,24 @@
 const Offer = require("../Models/Offer");
 
-const create = async (body, companyId) => {
+const getAll = () => Offer.find().select("-description -program");
+
+const getOne = (offerId) => Offer.findById(offerId);
+
+const create = (body, companyId) => {
   const offerData = {
     title: body.title,
     location: body.location,
     description: body.description,
     price: Number(body.price),
+    program: body.program,
     companyId,
   };
 
-  return await Offer.create(offerData);
+  return Offer.create(offerData);
 };
 
 module.exports = {
   create,
+  getAll,
+  getOne,
 };
