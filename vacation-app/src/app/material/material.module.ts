2ff10bcf9a30471f36d322f2680a6ea1869106b6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-04 13:04:46 +0200

    add some styles

---
+++ vacation-app/src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  imports: [CommonModule, MatIconModule],
+  exports: [MatIconModule],
+})
+export class MaterialModule {}
