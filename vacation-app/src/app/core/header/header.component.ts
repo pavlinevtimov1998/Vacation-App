333e1fa7a8d311c7b7feb2d5db94bcf9ed9fa193
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-06 00:56:37 +0200

    add mobile nav

--- vacation-app/src/app/core/header/header.component.ts
+++ vacation-app/src/app/core/header/header.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openMobileNav(aside: HTMLElement) {
+    aside.style.right = '0px';
   }
 
+  closeMobileNav(aside: HTMLElement) {
+    aside.style.right = '-300px';
+  }
 }
--- vacation-app/src/app/material/material.module.ts
+++ vacation-app/src/app/material/material.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule],
-  exports: [MatIconModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
+  exports: [MatIconModule, MatButtonModule],
 })
 export class MaterialModule {}
