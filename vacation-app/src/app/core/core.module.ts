fecdb40778de120f6409888e1f3276ce2993fa34
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-05 23:15:55 +0200

    add search component

--- vacation-app/src/app/core/core.module.ts
+++ vacation-app/src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from '../material/material.module';
 import { RouterModule } from '@angular/router';
 import { CarouselComponent } from './carousel/carousel.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [HeaderComponent, CarouselComponent],
+  declarations: [HeaderComponent, CarouselComponent, SearchComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
   exports: [HeaderComponent, CarouselComponent],
 })
