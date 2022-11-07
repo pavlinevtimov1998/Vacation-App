2f49cfa59023919afc5fe3a101c88dddfabd3e19
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-07 23:16:12 +0200

    Create app-auth-routing.module.ts

---
+++ vacation-app/src/app/auth/app-auth-routing.module.ts
@@ -0,0 +1,21 @@
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+];
+
+export const authRoutingModule = RouterModule.forChild(routes);
