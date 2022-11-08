18994b211b9634aef5a7adf0285fb1f8e8a6a0ed
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-08 12:20:34 +0200

    finish login page

--- vacation-app/src/app/auth/auth.module.ts
+++ vacation-app/src/app/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
+import { authRoutingModule } from './app-auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, ProfileComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, authRoutingModule, MaterialModule],
   exports: [LoginComponent, RegisterComponent, ProfileComponent],
 })
 export class AuthModule {}
--- vacation-app/src/app/auth/login/login.component.ts
+++ vacation-app/src/app/auth/login/login.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleOptionBtn(
+    addClass: HTMLElement,
+    removeClass: HTMLElement,
+    hide: HTMLElement,
+    show: HTMLElement
+  ) {
+    addClass.classList.add('selected');
+    removeClass.classList.remove('selected');
+
+    hide.style.display = 'none';
+    show.style.display = 'block';
   }
-
 }
