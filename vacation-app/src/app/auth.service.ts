394d337a3937459416a233bd0ba02befe13c5427
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-09 23:58:21 +0200

    Update auth.service.ts

--- vacation-app/src/app/auth.service.ts
+++ vacation-app/src/app/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { IUser } from './core/interfaces/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  userRegister$(body: {
+    username: string;
+    password: string;
+    rePassword: string;
+  }): Observable<IUser> {
+    return this.httpClient.post<IUser>(environment.api + 'user/register', body, {
+      withCredentials: true,
+    });
+  }
+
+  userLogin$(body: { username: string; password: string }): Observable<IUser> {
+    return this.httpClient.post<IUser>(environment.api + 'user/login', body, {
+      withCredentials: true,
+    });
+  }
 }
