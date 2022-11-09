4d02c8c17f423f77b5330cde218dee4bd66c48c9
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-09 23:32:40 +0200

    Create Agency.js

---
+++ Rest-Api/Models/Agency.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const agencySchema = new mongoose.Schema({
+  agencyName: {
+    type: String,
+    required: [true, "Agency name is required!"],
+    minLength: [4, "Agency name should be at least 2 characters!"],
+    maxLength: [25, "Agency name should be max 25 characters!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+    minLength: [10, "Email should be at least 10 characters!"],
+    maxLength: [30, "Email should be max 30 characters!"],
+  },
+  password: {
+    type: String,
+    minLength: [4, "Password should be at least 4 characters!"],
+    maxLength: [20, "Password should be max 20 characters!"],
+    required: [true, "Password is required!"],
+  },
+});
+
+agencySchema.pre("save", async function (next) {
+  const hashedPassword = await bcrypt.hash(
+    this.password,
+    Number(process.env.SALT)
+  );
+
+  this.password = hashedPassword;
+
+  next();
+});
+
+const Agency = mongoose.model("Agency", agencySchema);
+
+module.exports = Agency;
