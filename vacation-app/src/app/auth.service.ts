4ec9695e31df051d125299713c845fdd294d3894
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 00:01:46 +0200

    Update auth.service.ts

--- vacation-app/src/app/auth.service.ts
+++ vacation-app/src/app/auth.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { IAgency } from './core/interfaces/agency.interface';
 
-import { IUser } from './core/interfaces/user';
+import { IUser } from './core/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -16,14 +17,46 @@ export class AuthService {
     password: string;
     rePassword: string;
   }): Observable<IUser> {
-    return this.httpClient.post<IUser>(environment.api + 'user/register', body, {
-      withCredentials: true,
-    });
+    return this.httpClient.post<IUser>(
+      environment.api + 'user/register',
+      body,
+      {
+        withCredentials: true,
+      }
+    );
   }
 
   userLogin$(body: { username: string; password: string }): Observable<IUser> {
     return this.httpClient.post<IUser>(environment.api + 'user/login', body, {
       withCredentials: true,
     });
+  }
+
+  agencyRegister$(body: {
+    email: string;
+    agencyName: string;
+    password: string;
+    rePassword: string;
+  }): Observable<IAgency> {
+    return this.httpClient.post<IAgency>(
+      environment.api + 'agency/register',
+      body,
+      {
+        withCredentials: true,
+      }
+    );
+  }
+
+  loginRegister$(body: {
+    email: string;
+    password: string;
+  }): Observable<IAgency> {
+    return this.httpClient.post<IAgency>(
+      environment.api + 'agency/login',
+      body,
+      {
+        withCredentials: true,
+      }
+    );
   }
 }
