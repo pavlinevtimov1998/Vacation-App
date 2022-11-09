b9ac3690e732edca0ff1d8107ee4efdd29533db1
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-09 23:44:13 +0200

    Update router.js

--- Rest-Api/router.js
+++ Rest-Api/router.js
@@ -4,8 +4,9 @@ const userController = require("./Controllers/userController");
 const agencyController = require("./Controllers/agencyController");
 const offerController = require("./Controllers/offerController");
 
-router.use("/auth/user", userController);
-router.use("/auth/agency", agencyController);
+router.use("/user", userController);
+router.use("/agency", agencyController);
+
 router.use("/data", offerController);
 
 module.exports = router;
