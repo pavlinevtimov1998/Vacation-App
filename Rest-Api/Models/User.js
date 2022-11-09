331c1c1be508e8bb313262c64d005cc187c0a373
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-09 23:41:39 +0200

    Update User.js

--- Rest-Api/Models/User.js
+++ Rest-Api/Models/User.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    // required: [true, "Username is required!"],
-    // minLength: [2, "Username should be at least 2 characters!"],
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      // required: [true, "Username is required!"],
+      // minLength: [2, "Username should be at least 2 characters!"],
+    },
+    email: {
+      type: String,
+      // required: [true, "Email is required!"],
+      // minLength: [10, "Email should be at least 10 characters!"],
+    },
+    password: {
+      type: String,
+      // minLength: [4, "Password should be at least 4 characters!"],
+      // required: [true, "Password is required!"],
+    },
+    photo: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  email: {
-    type: String,
-    // required: [true, "Email is required!"],
-    // minLength: [10, "Email should be at least 10 characters!"],
-  },
-  password: {
-    type: String,
-    // minLength: [4, "Password should be at least 4 characters!"],
-    // required: [true, "Password is required!"],
-  },
-  photo: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
-});
+  { timestamps: { createdAt: "createdAt" } }
+);
 
 userSchema.pre("save", async function (next) {
   const hashedPassword = await bcrypt.hash(
