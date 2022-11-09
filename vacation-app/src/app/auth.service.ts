831c2c97ecd02437e8acad80cf9b53679a3c196a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 00:31:16 +0200

    Update auth.service.ts

--- vacation-app/src/app/auth.service.ts
+++ vacation-app/src/app/auth.service.ts
@@ -1,16 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+
 import { environment } from 'src/environments/environment';
 import { IAgency } from './core/interfaces/agency.interface';
-
 import { IUser } from './core/interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  guest!: IUser | IAgency;
+
+  private _currentUser = new BehaviorSubject(this.guest);
+
+  currentUser$ = this._currentUser.asObservable();
+  islogged$ = this._currentUser.pipe(map((user) => !user));
+
   constructor(private httpClient: HttpClient) {}
+
+  loginHandler(user: IUser | IAgency) {
+    console.log(user);
+
+    this._currentUser.next(user);
+  }
 
   userRegister$(body: {
     username: string;
@@ -57,6 +70,13 @@ export class AuthService {
       {
         withCredentials: true,
       }
+    );
+  }
+
+  logout$(): Observable<{ message: string }> {
+    return this.httpClient.delete<{ message: string }>(
+      environment.api + 'logout',
+      { withCredentials: true }
     );
   }
 }
