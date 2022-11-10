dc1f95e77bb5a74b3526a4c09a9dc85fac3cfce4
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 23:15:12 +0200

    Update user-login.component.ts

--- vacation-app/src/app/auth/user-login/user-login.component.ts
+++ vacation-app/src/app/auth/user-login/user-login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { AuthService } from 'src/app/auth.service';
+import { errorHandler } from '../../util/form-errors';
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent implements OnInit {
+  userLoginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get errHandler() {
+    return errorHandler;
   }
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.userLoginForm = this.formBuilder.group({
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  handleUserLogin() {
+    if (this.userLoginForm.invalid) {
+      return this.userLoginForm.markAllAsTouched();
+    }
+  }
 }
