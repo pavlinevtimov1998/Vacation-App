db68693c1199a79e3d9702856836dbc2246672e1
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 13:32:32 +0200

    add passwords mismatch validator

--- vacation-app/src/app/auth/user-register/user-register.component.ts
+++ vacation-app/src/app/auth/user-register/user-register.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormBuilder,
   FormControl,
   FormGroup,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 
@@ -33,10 +36,25 @@ export class UserRegisterComponent implements OnInit {
       username: new FormControl(null, [Validators.required]),
       passwords: new FormGroup({
         password: this.password,
-        rePassword: new FormControl(null, [Validators.required]),
+        rePassword: new FormControl(null, [
+          Validators.required,
+          this.passwordsMismatch(this.password),
+        ]),
       }),
     });
   }
 
   handleUserRegister() {}
+
+  private passwordsMismatch(password: AbstractControl): ValidatorFn {
+    return (rePass: AbstractControl): ValidationErrors | null => {
+      if (rePass.value !== password.value) {
+        return {
+          notMatch: true,
+        };
+      }
+
+      return null;
+    };
+  }
 }
