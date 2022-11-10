68c225591c26b546c2a0ce09c57d808dfe121158
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 14:06:36 +0200

    Update auth.module.ts

--- vacation-app/src/app/auth/auth.module.ts
+++ vacation-app/src/app/auth/auth.module.ts
@@ -9,9 +9,23 @@ import { UserLoginComponent } from './user-login/user-login.component';
 import { AgencyLoginComponent } from './agency-login/agency-login.component';
 import { AgencyRegisterComponent } from './agency-register/agency-register.component';
 import { UserRegisterComponent } from './user-register/user-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, ProfileComponent, UserLoginComponent, AgencyLoginComponent, AgencyRegisterComponent, UserRegisterComponent],
-  imports: [CommonModule, authRoutingModule, MaterialModule],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    UserLoginComponent,
+    AgencyLoginComponent,
+    AgencyRegisterComponent,
+    UserRegisterComponent,
+  ],
+  imports: [
+    CommonModule,
+    authRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
 })
 export class AuthModule {}
