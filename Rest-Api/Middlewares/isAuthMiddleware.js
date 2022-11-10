db9b22051a00aefa46962a92a3139b470667f673
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 23:43:20 +0200

    Update isAuthMiddleware.js

--- Rest-Api/Middlewares/isAuthMiddleware.js
+++ Rest-Api/Middlewares/isAuthMiddleware.js
@@ -1,24 +1,27 @@
 const { catchAsyncError } = require("../Util/errorParser");
 const { verifyToken } = require("../util/jwtConfig");
 
-exports.isAuth = catchAsyncError(async (req, res, next) => {
+exports.isAuth = async (req, res, next) => {
   const token = req.cookies[process.env.COOKIE_NAME];
 
   if (!token) {
     return next();
   }
 
-  const payload = await verifyToken(token);
+  try {
+    const payload = await verifyToken(token);
 
-  if (!payload) {
-    return next();
+    if (!payload) {
+      return next();
+    }
+
+    if (payload.agencyName) {
+      req.agency = payload;
+    } else {
+      req.user = payload;
+    }
+  } catch (err) {
+    res.clearCookie(process.env.COOKIE_NAME);
+    res.status(401).json({});
   }
-
-  if (payload.agencyName) {
-    req.agency = payload;
-  } else {
-    req.user = payload;
-  }
-
-  next();
-});
+};
