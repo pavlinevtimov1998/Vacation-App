60cad970823001d838a56ae5ad8ebfcb43955e14
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 13:23:43 +0200

    Update user-register.component.ts

--- vacation-app/src/app/auth/user-register/user-register.component.ts
+++ vacation-app/src/app/auth/user-register/user-register.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-user-register',
   templateUrl: './user-register.component.html',
-  styleUrls: ['./user-register.component.css']
+  styleUrls: ['./user-register.component.css'],
 })
 export class UserRegisterComponent implements OnInit {
+  userRegisterForm!: FormGroup;
+  password!: FormControl;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get passwordsGroup() {
+    return this.userRegisterForm.controls['passwords'] as FormGroup;
   }
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.password = new FormControl(null, [Validators.required]);
+
+    this.userRegisterForm = this.formBuilder.group({
+      username: new FormControl(null, [Validators.required]),
+      passwords: new FormGroup({
+        password: this.password,
+        rePassword: new FormControl(null, [Validators.required]),
+      }),
+    });
+  }
+
+  handleUserRegister() {}
 }
