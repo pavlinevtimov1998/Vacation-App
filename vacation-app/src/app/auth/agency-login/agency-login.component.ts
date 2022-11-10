a615b64ed2b66e6b257241d8dce876c86e3d436d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-10 23:14:41 +0200

    add user ligin with validation

--- vacation-app/src/app/auth/agency-login/agency-login.component.ts
+++ vacation-app/src/app/auth/agency-login/agency-login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { AuthService } from 'src/app/auth.service';
+import { errorHandler } from '../../util/form-errors';
 
 @Component({
   selector: 'app-agency-login',
   templateUrl: './agency-login.component.html',
-  styleUrls: ['./agency-login.component.css']
+  styleUrls: ['./agency-login.component.css'],
 })
 export class AgencyLoginComponent implements OnInit {
+  agencyLoginForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get errHandler() {
+    return errorHandler;
   }
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.agencyLoginForm = this.formBuilder.group({
+      email: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  handleAgencyLogin() {
+    if (this.agencyLoginForm.invalid) {
+      return this.agencyLoginForm.markAllAsTouched();
+    }
+  }
 }
