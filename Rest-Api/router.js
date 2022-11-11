25ddc27f2bfaf5ed7b6f927636eac9d60219444c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-11 13:45:17 +0200

    add not found path

--- Rest-Api/router.js
+++ Rest-Api/router.js
@@ -7,6 +7,10 @@ const { globalErrorHandler } = require("./Middlewares/globallErrHandler");
 router.use("/auth", authController);
 router.use("/data", offerController);
 
+router.use("*", (req, res) => {
+  res.status(404).json({ message: "Not Found!" });
+});
+
 router.use(globalErrorHandler);
 
 module.exports = router;
