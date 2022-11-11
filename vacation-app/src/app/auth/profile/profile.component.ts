ba2c079ca2cd4368db729a2bd991259f55f4ed78
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-12 00:23:33 +0200

    Update profile.component.ts

--- vacation-app/src/app/auth/profile/profile.component.ts
+++ vacation-app/src/app/auth/profile/profile.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
+import { IAccount } from 'src/app/core/interfaces/account.interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, OnDestroy {
+  account!: IAccount;
 
-  constructor() { }
+  subscription!: Subscription;
+
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.subscription = this.authService.currentUser$.subscribe({
+      next: (account) => {
+        this.account = account;
+      },
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
