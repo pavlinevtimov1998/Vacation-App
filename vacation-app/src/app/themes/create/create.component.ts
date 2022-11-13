f0591b33474b37a5ee8bb8f6a303b2cad0f1d119
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-13 12:16:24 +0200

    add basic create offer validations

--- vacation-app/src/app/themes/create/create.component.ts
+++ vacation-app/src/app/themes/create/create.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { errorHandler } from 'src/app/util/form-errors';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
+  createOfferForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get errHandler() {
+    return errorHandler;
   }
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createOfferForm = this.formBuilder.group({
+      title: new FormControl(null, [Validators.required]),
+      description: new FormControl(null, [Validators.required]),
+      country: new FormControl(null, [Validators.required]),
+      town: new FormControl(null, [Validators.required]),
+      price: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  handleCreateOffer() {
+    if (this.createOfferForm.invalid) {
+      return this.createOfferForm.markAllAsTouched();
+    }
+  }
 }
--- vacation-app/src/app/themes/themes.module.ts
+++ vacation-app/src/app/themes/themes.module.ts
@@ -5,9 +5,20 @@ import { MaterialModule } from '../material/material.module';
 import { themesRoutingModule } from './app-themes-routing.module';
 import { TopAgenciesComponent } from './top-agencies/top-agencies.component';
 import { CreateComponent } from './create/create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [CountriesCatalogComponent, TopAgenciesComponent, CreateComponent],
-  imports: [CommonModule, MaterialModule, themesRoutingModule],
+  declarations: [
+    CountriesCatalogComponent,
+    TopAgenciesComponent,
+    CreateComponent,
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    themesRoutingModule,
+    ReactiveFormsModule,
+  ],
 })
 export class ThemesModule {}
