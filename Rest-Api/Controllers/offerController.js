0d712b43b2cad0ca1333a1d4cc71c0a18d72747a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-14 14:06:35 +0200

    Update offerController.js

--- Rest-Api/Controllers/offerController.js
+++ Rest-Api/Controllers/offerController.js
@@ -3,15 +3,17 @@ const offerController = require("express").Router();
 const { isAgency } = require("../Middlewares/guards");
 const offerService = require("../Services/offerService");
 const { catchAsyncError } = require("../Util/errorParser");
+const { upload } = require("../Util/imageUpload");
 
 offerController.post(
-  "/offer",
+  "/offers",
   isAgency(),
+  upload.array("images"),
   catchAsyncError(async (req, res) => {
     const body = req.body;
-    const agencyId = req.agency._id;
+    body.agencyId = req.agency._id;
 
-    const offer = await offerService.create(body, agencyId);
+    const offer = await offerService.createOffer(body, req.files);
 
     res.status(201).json(offer);
   })
