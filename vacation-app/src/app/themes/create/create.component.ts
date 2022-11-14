3709c4dda43e14e2ea91b9f6c46d7714534203a6
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-14 14:06:28 +0200

    add create offer form

--- vacation-app/src/app/themes/create/create.component.ts
+++ vacation-app/src/app/themes/create/create.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { OfferService } from 'src/app/offer.service';
 import { errorHandler } from 'src/app/util/form-errors';
 
 @Component({
@@ -19,7 +20,10 @@ export class CreateComponent implements OnInit {
     return errorHandler;
   }
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private offerService: OfferService
+  ) {}
 
   ngOnInit(): void {
     this.createOfferForm = this.formBuilder.group({
@@ -28,12 +32,48 @@ export class CreateComponent implements OnInit {
       country: new FormControl(null, [Validators.required]),
       town: new FormControl(null, [Validators.required]),
       price: new FormControl(null, [Validators.required]),
+      images: new FormControl(null, [Validators.required]),
     });
+  }
+
+  onImageUpload(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    const images: File[] = [];
+
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        images.push(files.item(i) as File);
+      }
+      this.createOfferForm.patchValue({
+        images: images,
+      });
+    }
   }
 
   handleCreateOffer() {
     if (this.createOfferForm.invalid) {
       return this.createOfferForm.markAllAsTouched();
     }
+
+    const formData = new FormData();
+
+    Object.entries(this.createOfferForm.value).forEach(([key, value]) => {
+      if (key == 'images') {
+        this.createOfferForm.value['images'].forEach((image: File) => {
+          formData.append(key, image, image.name);
+        });
+      } else {
+        formData.append(key, value as string);
+      }
+    });
+
+    this.offerService.createOffer(formData).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 }
