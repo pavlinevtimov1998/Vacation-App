fd1ef8d8ede2ab8ae6b254f6b243ecf1922e5256
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-14 14:51:24 +0200

    Update imageUpload.js

--- Rest-Api/Util/imageUpload.js
+++ Rest-Api/Util/imageUpload.js
@@ -12,11 +12,5 @@ const storage = multer.diskStorage({
 
 exports.upload = multer({ storage: storage });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
-});
-
 exports.uploadToCloudinary = async (filePath) =>
   cloudinary.uploader.upload(filePath);
