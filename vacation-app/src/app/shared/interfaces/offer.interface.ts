192a9cec021a8c0fcf64a845ff70c54f32f248b4
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-14 14:06:17 +0200

    Create offer.interface.ts

---
+++ vacation-app/src/app/shared/interfaces/offer.interface.ts
@@ -0,0 +1,12 @@
+export interface IOffer {
+  title: string;
+  town: string;
+  country: string;
+  description: string;
+  price: number;
+  images: string[];
+  ratingsQuantity: string;
+  rating: string;
+  peopleBooked: number;
+  agencyId: string;
+}
