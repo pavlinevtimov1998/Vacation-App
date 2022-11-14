64b7a53bc8ca763842c66bb8e6153cad901703c0
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-14 14:06:39 +0200

    Create Country.js

---
+++ Rest-Api/Models/Country.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const countrySchema = new mongoose.Schema({
+  country: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: [true, "Country is required!"],
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  offersId: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Offer",
+    },
+  ],
+});
+
+const Country = mongoose.model("Country", countrySchema);
+
+module.exports = Country;
