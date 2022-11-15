dd74e8005121c87a59c61b056b2c4a160cd9b8b0
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-15 13:49:59 +0200

    change paths

--- Rest-Api/router.js
+++ Rest-Api/router.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 
 const authController = require("./Controllers/authController");
+const countryController = require("./Controllers/countryController");
 const offerController = require("./Controllers/offerController");
 const welcomeController = require("./Controllers/welcomeController");
 const { globalErrorHandler } = require("./Middlewares/globallErrHandler");
 
 router.use("/", welcomeController);
 router.use("/auth", authController);
-router.use("/data", offerController);
+router.use("/countries", countryController);
+router.use("/offers", offerController);
 
 router.use("*", (req, res) => {
   res.status(404).json({ message: "Not Found!" });
