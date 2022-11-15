b62cda899b5409519dd113fa9f4c3f72e6d75874
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-15 22:37:55 +0200

    Create userController.js

---
+++ Rest-Api/Controllers/userController.js
@@ -0,0 +1,38 @@
+const userController = require("express").Router();
+
+const userService = require("../Services/userService");
+const { catchAsyncError } = require("../Util/errorParser");
+const { isGuest, isAccount } = require("../Middlewares/guards");
+
+userController.post(
+  "/register",
+  isGuest(),
+  catchAsyncError(async (req, res) => {
+    const [token, user] = await userService.register(req.body);
+
+    res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true });
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+    });
+  })
+);
+
+userController.post("/login", isGuest(), async (req, res) => {
+  const [token, user] = await userService.login(req.body);
+
+  res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true });
+
+  res.status(200).json({
+    _id: user._id,
+    username: user.username,
+  });
+});
+
+userController.get("/logout", isAccount(), (req, res) => {
+  res.clearCookie(process.env.COOKIE_NAME);
+  res.status(200).json({ message: "Successfull logout!" });
+});
+
+module.exports = userController;
