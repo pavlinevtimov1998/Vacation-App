a2cba6ffdd476589228283226383d40e22ef8f9a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-15 22:37:58 +0200

    Update router.js

--- Rest-Api/router.js
+++ Rest-Api/router.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 
-const authController = require("./Controllers/authController");
+const userController = require("./Controllers/userController");
 const countryController = require("./Controllers/countryController");
 const offerController = require("./Controllers/offerController");
 const welcomeController = require("./Controllers/welcomeController");
+const agencyController = require("./Controllers/agencyController");
+
 const { globalErrorHandler } = require("./Middlewares/globallErrHandler");
 
 router.use("/", welcomeController);
-router.use("/auth", authController);
+router.use("/user", userController);
+router.use("/agency", agencyController);
 router.use("/countries", countryController);
 router.use("/offers", offerController);
 
