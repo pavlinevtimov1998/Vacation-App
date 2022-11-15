a40c86a4516c27a721f0ab4c8550e8cad9aae195
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-15 13:13:15 +0200

    Update welcomeController.js

--- Rest-Api/Controllers/welcomeController.js
+++ Rest-Api/Controllers/welcomeController.js
@@ -1,33 +1,19 @@
 const welcomeController = require("express").Router();
 
-const offerService = require("../Services/offerService");
+const welcomeService = require("../Services/welcomeService");
 const { catchAsyncError } = require("../Util/errorParser");
 
 welcomeController.get(
   "/",
   catchAsyncError(async (req, res) => {
-    const topOffers = await offerService.topOffers();
+    const topOffers = await welcomeService.topOffers();
 
     res.status(200).json(topOffers);
   })
 );
 
-const mockData = [
-  {
-    country: "Spain",
-    imgUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461400/spain_vuznoh.jpg",
-  },
-  {
-    country: "France",
-    imgUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461422/france_fum18o.jpg",
-  },
-  {
-    country: "Greece",
-    imgUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461413/greece_gmgi32.jpg",
-  },
-];
+welcomeController.get("/top-countries", (req, res) => {
+  res.status(200).json(welcomeService.mockData);
+});
 
 module.exports = welcomeController;
