9c7cb8c75e53e4cf9834b801a0381359085a0723
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-16 23:17:57 +0200

    Update welcomeService.js

--- Rest-Api/Services/welcomeService.js
+++ Rest-Api/Services/welcomeService.js
@@ -1,3 +1,4 @@
+const Country = require("../Models/Country");
 const Offer = require("../Models/Offer");
 
 const topOffers = () =>
@@ -9,28 +10,9 @@ const topOffers = () =>
     )
     .populate("agencyId", "-password -email -updatedAt -createdAt -__v");
 
-const mockData = [
-  {
-    country: "Spain",
-    id: "63722dd7bb32f901346fd4ad",
-    imageUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461400/spain_vuznoh.jpg",
-  },
-  {
-    country: "France",
-    id: "6372359c40b9485b69897803",
-    imageUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461422/france_fum18o.jpg",
-  },
-  {
-    country: "Greece",
-    id: "",
-    imageUrl:
-      "https://res.cloudinary.com/dlw7pmlvx/image/upload/v1668461413/greece_gmgi32.jpg",
-  },
-];
+const TopCointries = () => Country.find();
 
 module.exports = {
   topOffers,
-  mockData,
+  TopCointries,
 };
