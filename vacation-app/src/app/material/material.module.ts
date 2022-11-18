efb3bea3b93577ff73beff75659518620f6e83dc
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-18 22:52:36 +0200

    Update material.module.ts

--- vacation-app/src/app/material/material.module.ts
+++ vacation-app/src/app/material/material.module.ts
@@ -3,9 +3,29 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPseudoCheckboxModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatDialogModule],
-  exports: [MatIconModule, MatIconModule, MatButtonModule, MatDialogModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatSelectModule,
+  ],
 })
 export class MaterialModule {}
