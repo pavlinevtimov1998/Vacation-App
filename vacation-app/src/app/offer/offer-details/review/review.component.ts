53e6db28259a866632e7b44d024e2709d11c0f19
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-20 23:42:06 +0200

    add review component with basic rating system

---
+++ vacation-app/src/app/offer/offer-details/review/review.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review',
+  templateUrl: './review.component.html',
+  styleUrls: ['./review.component.css'],
+})
+export class ReviewComponent implements OnInit {
+  rates = [1, 2, 3, 4, 5];
+
+  constructor() {}
+
+  rate = 0;
+
+  ngOnInit(): void {}
+
+  rateHandler(starsContainer: HTMLDivElement, index: number, rate: number) {
+    for (let i = 0; i < starsContainer.children.length; i++) {
+      if (i <= index) {
+        starsContainer.children[i].textContent = 'star';
+      } else {
+        starsContainer.children[i].textContent = 'star_border';
+      }
+    }
+
+    this.rate = rate;
+  }
+}
