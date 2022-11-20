483c2efe7ab5cc05bf81859eaaa26061bef462e3
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-21 01:25:26 +0200

    Update review.component.ts

--- vacation-app/src/app/offer/offer-details/review/review.component.ts
+++ vacation-app/src/app/offer/offer-details/review/review.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-review',
@@ -25,4 +25,6 @@ export class ReviewComponent implements OnInit {
 
     this.rate = rate;
   }
+
+  closeReviewContainer() {}
 }
