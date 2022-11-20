1608085823cd480ae8b66787089553e50e229d1c
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-20 11:06:32 +0200

    Update countryService.js

--- Rest-Api/Services/countryService.js
+++ Rest-Api/Services/countryService.js
@@ -1,9 +1,19 @@
 const Country = require("../Models/Country");
 
-const getAll = () => Country.find();
+const getAll = () => Country.find().sort({ name: -1 });
 
 const getCountryOffers = (countryId) =>
-  Country.findById(countryId).select("-image").populate("offersId");
+  Country.findById(countryId)
+    .select("-image")
+    .populate({
+      path: "offers",
+      select:
+        "-description -__v -ratingsQuantity -rating -peopleBooked -createdAt",
+      populate: {
+        path: "country",
+        select: "-image",
+      },
+    });
 
 module.exports = {
   getCountryOffers,
