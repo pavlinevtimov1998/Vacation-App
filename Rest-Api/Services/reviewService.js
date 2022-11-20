5db88031f5e5b3540c4dac62698e984744be1400
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-20 18:57:02 +0200

    Update reviewService.js

--- Rest-Api/Services/reviewService.js
+++ Rest-Api/Services/reviewService.js
@@ -1,3 +1,14 @@
 const Review = require("../Models/Review");
 
-const getReview = () => Review.find();
+const getOfferReviews = (offerId) =>
+  Review.find({ offer: offerId }).populate({
+    path: "user",
+    select: "-password -createdAt -updatedAt -__v -bookedOffers",
+  });
+
+const addReview = (body) => Review.create(body);
+
+module.exports = {
+  getOfferReviews,
+  addReview,
+};
