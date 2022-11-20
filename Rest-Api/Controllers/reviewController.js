061c4552be507d84b5e59bc47b4fc7a871b86edb
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-20 18:56:56 +0200

    Update reviewController.js

--- Rest-Api/Controllers/reviewController.js
+++ Rest-Api/Controllers/reviewController.js
@@ -1,6 +1,33 @@
-const reviewController = require('express').Router();
+const reviewController = require("express").Router();
 
+const reviewService = require("../Services/reviewService");
 
-// reviewController.post('/')
+const { isUser } = require("../Middlewares/guards");
+const { catchAsyncError } = require("../Util/errorParser");
+
+reviewController.get(
+  "/:offerId",
+  catchAsyncError(async (req, res) => {
+    const offerId = req.params.offerId;
+
+    const reviews = await reviewService.getOfferReviews(offerId);
+
+    res.status(200).json(reviews);
+  })
+);
+
+reviewController.post(
+  "/:offerId",
+  isUser(),
+  catchAsyncError(async (req, res) => {
+    const body = req.body;
+    body.offer = req.params.offerId;
+    body.user = req.user._id;
+
+    await reviewService.review(body);
+
+    res.status(200).json({ message: "Successfull review!" });
+  })
+);
 
 module.exports = reviewController;
