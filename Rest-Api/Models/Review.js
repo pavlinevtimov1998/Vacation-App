4de5e4d0d6d027aa14a08d2fdef603ded354e75e
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-20 18:56:57 +0200

    Update Review.js

--- Rest-Api/Models/Review.js
+++ Rest-Api/Models/Review.js
@@ -6,6 +6,10 @@ const reviewSchema = new mongoose.Schema(
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
+    offer: {
+      type: mongoose.Types.ObjectId,
+      ref: "Offer",
+    },
     rating: {
       type: Number,
       required: true,
