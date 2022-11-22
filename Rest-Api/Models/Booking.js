d64a48dca3af219fcd6b80db9bf17069d7fbf37b
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-22 14:23:06 +0200

    Create Booking.js

---
+++ Rest-Api/Models/Booking.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const bookinkSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    agency: {
+      type: mongoose.Types.ObjectId,
+      ref: "Agency",
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    cost: {
+      type: Number,
+      required: true,
+    },
+    offer: {
+      type: mongoose.Types.ObjectId,
+      ref: "Offer",
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+    },
+  }
+);
+
+const Booking = mongoose.model("Booking", bookinkSchema);
+
+module.exports = Booking;
