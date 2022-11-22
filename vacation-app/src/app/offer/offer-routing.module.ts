ed37a4d0b479d11f437b3eaf8e26c2f080d160dc
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-23 00:42:17 +0200

    Update offer-routing.module.ts

--- vacation-app/src/app/offer/offer-routing.module.ts
+++ vacation-app/src/app/offer/offer-routing.module.ts
@@ -2,6 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from '../offer/create/create.component';
 import { OfferCatalogComponent } from '../offer/offer-catalog/offer-catalog.component';
 import { OfferDetailsComponent } from '../offer/offer-details/offer-details.component';
+import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'create',
     component: CreateComponent,
+  },
+  {
+    path: 'book/:offerId',
+    component: BookingComponent,
   },
   {
     path: ':offerId',
