f1bcf29acb49a1c516a29b9a2f963876b4f3b765
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-22 14:23:05 +0200

    Update globallErrHandler.js

--- Rest-Api/Middlewares/globallErrHandler.js
+++ Rest-Api/Middlewares/globallErrHandler.js
@@ -1,5 +1,5 @@
 exports.globalErrorHandler = (err, req, res, next) => {
-  console.log(err);
+  console.log(err, 'From global error handler!');
   res.status(err.status).json({
     message: err.message,
   });
