9d2187b33980e0c409e321ec03fd3b22293279d9
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-23 14:12:56 +0200

    Update bookingController.js

--- Rest-Api/Controllers/bookingController.js
+++ Rest-Api/Controllers/bookingController.js
@@ -1,6 +1,21 @@
-const bookingController = require('express').Router();
+const bookingController = require("express").Router();
 
+const bookingService = require("../Services/bookingService");
+const { isUser } = require("../Middlewares/guards");
+const { catchAsyncError } = require("../Util/errorParser");
 
+bookingController.post(
+  "/:offerId",
+  isUser(),
+  catchAsyncError(async (req, res) => {
+    const body = req.body;
+    body.offer = req.params.offerId;
+    body.user = req.user._id;
 
+    const bookingData = await bookingService.booking(body);
+
+    res.json(bookingData);
+  })
+);
 
 module.exports = bookingController;
