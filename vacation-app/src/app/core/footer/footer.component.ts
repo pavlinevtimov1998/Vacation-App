b8ce189a00e1be736d1a7ee86faad91c33cbc80d
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-24 00:13:18 +0200

    Update footer.component.ts

--- vacation-app/src/app/core/footer/footer.component.ts
+++ vacation-app/src/app/core/footer/footer.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthDialogComponent } from 'src/app/shared/dialog/auth-dialog.component';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
+  constructor(private dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signUpDialogHandler() {
+    this.dialog.open(AuthDialogComponent, {
+      width: '400px',
+      height: '300px',
+    });
   }
-
 }
