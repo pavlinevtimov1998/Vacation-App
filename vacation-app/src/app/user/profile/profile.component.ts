6224db991a165250546966a7dbb8b0a20d4e08ea
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-24 14:18:29 +0200

    Update profile.component.ts

--- vacation-app/src/app/user/profile/profile.component.ts
+++ vacation-app/src/app/user/profile/profile.component.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { IUser } from 'src/app/shared/interfaces';
+import { IBooking } from 'src/app/shared/interfaces/booking.interface';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
+  profileData!: IUser;
+  bookings!: IBooking[];
 
-  isLoading = false;
+  isLoading: boolean = true;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-    
+    this.userService.getUserProfileData$().subscribe({
+      next: ({ profileData, bookings }) => {
+        this.profileData = profileData;
+        this.bookings = bookings;
+        console.log(profileData);
+        
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.router.navigate(['/']);
+      },
+    });
   }
-
 }
