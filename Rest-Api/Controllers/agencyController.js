e0a7bc76efa0250b7c782b87653dafd3bd056e77
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-26 00:25:55 +0200

    Update agencyController.js

--- Rest-Api/Controllers/agencyController.js
+++ Rest-Api/Controllers/agencyController.js
@@ -20,21 +20,34 @@ agencyController.post(
   })
 );
 
-agencyController.post("/login", isGuest(), async (req, res) => {
-  const [token, agency] = await agencyService.agencyLogin(req.body);
+agencyController.post(
+  "/login",
+  isGuest(),
+  catchAsyncError(async (req, res) => {
+    const [token, agency] = await agencyService.agencyLogin(req.body);
 
-  res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true });
+    res.cookie(process.env.COOKIE_NAME, token, { httpOnly: true });
 
-  res.status(200).json({
-    _id: agency._id,
-    email: agency.email,
-    agencyName: agency.agencyName,
-  });
-});
+    res.status(200).json({
+      _id: agency._id,
+      email: agency.email,
+      agencyName: agency.agencyName,
+    });
+  })
+);
 
 agencyController.get("/logout", isAccount(), (req, res) => {
   res.clearCookie(process.env.COOKIE_NAME);
   res.status(200).json({ message: "Successfull logout!" });
 });
+
+agencyController.get(
+  "/top-agencies",
+  catchAsyncError(async (req, res) => {
+    const topAgencies = await agencyService.getTopAgencies();
+
+    res.status(200).json(topAgencies);
+  })
+);
 
 module.exports = agencyController;
