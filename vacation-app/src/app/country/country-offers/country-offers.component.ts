f38125cc83d402f9e897c9fd49942fa78fbe6b26
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-11-25 14:42:35 +0200

    Update country-offers.component.ts

--- vacation-app/src/app/country/country-offers/country-offers.component.ts
+++ vacation-app/src/app/country/country-offers/country-offers.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { mergeMap, Subscription } from 'rxjs';
+
+import { ICountry, IOffer } from 'src/app/shared/interfaces';
+import { CountryService } from '../country.service';
 
 @Component({
   selector: 'app-country-offers',
   templateUrl: './country-offers.component.html',
-  styleUrls: ['./country-offers.component.css']
+  styleUrls: ['./country-offers.component.css'],
 })
 export class CountryOffersComponent implements OnInit {
+  country!: ICountry;
+  offers!: IOffer[];
 
-  constructor() { }
+  subscription!: Subscription;
+
+  isLoading: boolean = true;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountryService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.subscription = this.activatedRoute.params
+      .pipe(
+        mergeMap((params) => {
+          const countryId = params['countryId'];
 
+          return this.countryService.getCountryWithOffers$(countryId);
+        })
+      )
+      .subscribe({
+        next: (country) => {
+          console.log(country);
+          
+          this.country = country;
+          this.offers = country.offers as IOffer[];
+
+          this.isLoading = false;
+        },
+        error: (err) => {
+          console.log(err);
+          this.router.navigate(['/']);
+        },
+      });
+  }
 }
