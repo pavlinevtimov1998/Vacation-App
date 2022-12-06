50febe2a4b87723e7d9e9e072ca3a14e47772f9e
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-12-06 22:38:12 +0200

    Update imageUpload.js

--- Rest-Api/Util/imageUpload.js
+++ Rest-Api/Util/imageUpload.js
@@ -1,5 +1,7 @@
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
+const { promisify } = require("util");
+const { unlink } = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -12,5 +14,27 @@ const storage = multer.diskStorage({
 
 exports.upload = multer({ storage: storage });
 
-exports.uploadToCloudinary = async (filePath) =>
+const uploadToCloudinary = async (filePath) =>
   cloudinary.uploader.upload(filePath);
+
+exports.getImagesUrl = async (files) => {
+  const imagesUrl = [];
+  const localUrls = [];
+
+  for (let i = 0; i < files.length; i++) {
+    const localFilePath = files[i].path;
+    localUrls.push(localFilePath);
+
+    await uploadToCloudinary(localFilePath).then((result) => {
+      imagesUrl.push(result.url);
+    });
+  }
+
+  return [imagesUrl, localUrls];
+};
+
+exports.asyncUnlink = (arr) => {
+  const func = promisify(unlink);
+
+  return arr.map((i) => func(i));
+};
