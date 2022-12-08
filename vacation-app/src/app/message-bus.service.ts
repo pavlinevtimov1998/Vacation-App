b46b4eee92a4d6da7aad589f8fd8f2cc67b76673
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-12-08 13:04:58 +0200

    add message bus service

---
+++ vacation-app/src/app/message-bus.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { IMessage } from './shared/interfaces/message.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageBusService {
+  private messageContainer$$ = new Subject<IMessage | undefined>();
+
+  newMessage$ = this.messageContainer$$.asObservable();
+
+  constructor() {}
+
+  addMessage(message: IMessage): void {
+    this.messageContainer$$.next(message);
+  }
+
+  clearMessage(): void {
+    this.messageContainer$$.next(undefined);
+  }
+}
