9d55f8148e080e05d76421130712655990a1e8f5
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-12-08 23:09:58 +0200

    Update country-offers.component.ts

--- vacation-app/src/app/country/country-offers/country-offers.component.ts
+++ vacation-app/src/app/country/country-offers/country-offers.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { mergeMap, Subscription } from 'rxjs';
+import { mergeMap, Subscription, switchMap } from 'rxjs';
 
 import { ICountry, IOffer } from 'src/app/shared/interfaces';
 import { CountryService } from '../country.service';
@@ -27,7 +27,7 @@ export class CountryOffersComponent implements OnInit, OnDestroy {
 
   isLoading = true;
 
-  subscription = new Subscription();
+  subscription!: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -36,53 +36,61 @@ export class CountryOffersComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.getCountry();
+    this.initialLoad();
   }
 
   setCurrentPage(currentPage: number) {
-    this.currentPage = currentPage;
+    if (this.currentPage !== currentPage) {
+      this.currentPage = currentPage;
+    }
+    this.subscription?.unsubscribe();
     this.getCountryOffers();
   }
 
   private getCountryOffers(): void {
     this.paginationLoading = true;
 
-    this.subscription.add(
-      this.countryService
-        .getCountryOffers$(this.countryId, this.skip, this.limit)
-        .subscribe({
-          next: (offers) => {
-            this.offers = offers;
-            this.paginationLoading = false;
-            this.isLoading = false;
-          },
-        })
-    );
+    this.subscription = this.countryService
+      .getCountryOffers$(this.countryId, this.skip, this.limit)
+      .subscribe({
+        next: (offers) => {
+          this.offers = offers;
+          this.paginationLoading = false;
+        },
+        error: (err) => {
+          this.router.navigate(['/']);
+        },
+      });
   }
 
-  private getCountry(): void {
-    this.subscription.add(
-      this.activatedRoute.params
-        .pipe(
-          mergeMap((params) => {
-            this.countryId = params['countryId'];
+  private initialLoad(): void {
+    this.subscription = this.activatedRoute.params
+      .pipe(
+        switchMap((params) => {
+          this.countryId = params['countryId'];
 
-            return this.countryService.getCountry$(this.countryId);
-          })
-        )
-        .subscribe({
-          next: ({ country, offersCount }) => {
-            this.pages = Math.ceil(offersCount / this.limit);
+          return this.countryService.getCountry$(this.countryId);
+        }),
+        mergeMap(({ country, offersCount }) => {
+          this.pages = Math.ceil(offersCount / this.limit);
+          this.country = country;
 
-            this.country = country;
-            this.getCountryOffers();
-          },
-          error: (err) => {
-            console.log(err);
-            this.router.navigate(['/']);
-          },
+          return this.countryService.getCountryOffers$(
+            this.countryId,
+            this.skip,
+            this.limit
+          );
         })
-    );
+      )
+      .subscribe({
+        next: (offers) => {
+          this.offers = offers;
+          this.isLoading = false;
+        },
+        error: (err) => {
+          this.router.navigate(['/']);
+        },
+      });
   }
 
   ngOnDestroy(): void {
