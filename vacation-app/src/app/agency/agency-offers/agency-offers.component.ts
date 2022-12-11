82878146f56e03ebcce91fbd1a207f2d247dd19a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-12-11 11:33:17 +0200

    Update agency-offers.component.ts

--- vacation-app/src/app/agency/agency-offers/agency-offers.component.ts
+++ vacation-app/src/app/agency/agency-offers/agency-offers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
+
 import { IAgency, IOffer } from 'src/app/shared/interfaces';
 import { AgencyService } from '../agency.service';
 
