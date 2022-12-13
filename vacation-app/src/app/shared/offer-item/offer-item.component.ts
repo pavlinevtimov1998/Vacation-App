d0631a8dc7d62fd5a253d99ff15ea4e930cf5e65
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-12-13 23:01:03 +0200

    update offer item

--- vacation-app/src/app/shared/offer-item/offer-item.component.ts
+++ vacation-app/src/app/shared/offer-item/offer-item.component.ts
@@ -1,12 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { combineLatest, map, Observable } from 'rxjs';
 
 import { AuthService } from 'src/app/auth/auth.service';
-import { MessageBusService } from 'src/app/message-bus.service';
-import { OfferService } from 'src/app/offer/offer.service';
 import { IOffer } from 'src/app/shared/interfaces';
-import { MessageType } from '../interfaces';
 
 @Component({
   selector: 'app-offer-item',
@@ -26,64 +21,5 @@ export class OfferItemComponent {
 
   isLoading = false;
 
-  constructor(
-    private authService: AuthService,
-    private router: Router,
-    private offerService: OfferService,
-    private messageBus: MessageBusService
-  ) {}
-
-  routeHandler(event: Event) {
-    if (
-      (event.target as HTMLElement).tagName !== 'BUTTON' &&
-      (event.target as HTMLElement).tagName !== 'MAT-ICON'
-    ) {
-      this.router.navigate([`/details${this.offer._id}`]);
-    }
-  }
-
-  addToFavorites() {
-    this.isLoading = true;
-    this.offerService.addToFavourites$(this.offer._id).subscribe({
-      next: ({ message, userId }) => {
-        this.offer.peopleFavourite.push(userId);
-
-        this.messageBus.addMessage({
-          message: 'Successfully added to favourites!',
-          type: MessageType.Success,
-        });
-
-        this.isLoading = false;
-      },
-      error: (err) => {
-        this.router.navigate(['/']);
-      },
-    });
-  }
-
-  removeFromFavorites() {
-    this.isLoading = true;
-    this.offerService.removeFromFavourites$(this.offer._id).subscribe({
-      next: ({ message, userId }) => {
-        this.offer.peopleFavourite = this.offer.peopleFavourite.filter(
-          (id) => id !== userId
-        );
-
-        this.messageBus.addMessage({
-          message: 'Successfully removed from favourites!',
-          type: MessageType.Success,
-        });
-        this.isLoading = false;
-      },
-      error: (err) => {
-        this.router.navigate(['/']);
-      },
-    });
-  }
-
-  isLiked(): Observable<boolean> {
-    return this.currentUser$.pipe(
-      map((user) => !!this.offer.peopleFavourite.find((id) => id == user?._id))
-    );
-  }
+  constructor(private authService: AuthService) {}
 }
