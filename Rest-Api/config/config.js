fd6bc52931da7dd62d4ec7ad8e4ca95e4f6e6e85
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2023-01-14 12:53:13 +0200

    Update config.js

--- Rest-Api/config/config.js
+++ Rest-Api/config/config.js
@@ -1,36 +1,36 @@
 const env = process.env.NODE_ENV || "DEVELOPMENT";
 
 const config = {
-  DEVELOPMENT: {
-    PORT: process.env.PORT || 3000,
-    DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS), // enter your local MongoDB string here
-    COOKIE_NAME: process.env.COOKIE_NAME || "token", // enter your cookie name here
-    SECRET: process.env.JWT_SECRET || "very secret word", //enter your secret here
-    SALT_ROUNDS: process.env.SALT || 10,
-    CLOUDINARY_CONFIG: {
-      cloud_name: process.env.CLOUD_NAME, // enter your cloud name from cloudinary account
-      api_key: process.env.API_KEY, // enter your cloud key from cloudinary account
-      api_secret: process.env.API_SECRET, // enter your cloud secret from cloudinary account
+    DEVELOPMENT: {
+        PORT: process.env.PORT || 3000,
+        DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS), // enter your local MongoDB string here
+        COOKIE_NAME: process.env.COOKIE_NAME || "token", // enter your cookie name here
+        SECRET: process.env.JWT_SECRET || "very secret word", //enter your secret here
+        SALT_ROUNDS: process.env.SALT || 10,
+        CLOUDINARY_CONFIG: {
+            cloud_name: process.env.CLOUD_NAME, // enter your cloud name from cloudinary account
+            api_key: process.env.API_KEY, // enter your cloud key from cloudinary account
+            api_secret: process.env.API_SECRET, // enter your cloud secret from cloudinary account
+        },
+        CORS: {
+            origin: "http://localhost:4200",
+        },
     },
-    CORS: {
-      origin: "http://localhost:4200",
+    PRODUCTION: {
+        PORT: process.env.PORT,
+        DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS),
+        COOKIE_NAME: process.env.COOKIE_NAME,
+        SECRET: process.env.JWT_SECRET,
+        SALT_ROUNDS: process.env.SALT,
+        CLOUDINARY_CONFIG: {
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET,
+        },
+        CORS: {
+            origin: "https://find-vacation.herokuapp.com",
+        },
     },
-  },
-  PRODUCTION: {
-    PORT: process.env.PORT,
-    DB_URL: process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASS),
-    COOKIE_NAME: process.env.COOKIE_NAME,
-    SECRET: process.env.JWT_SECRET,
-    SALT_ROUNDS: process.env.SALT,
-    CLOUDINARY_CONFIG: {
-      cloud_name: process.env.CLOUD_NAME,
-      api_key: process.env.API_KEY,
-      api_secret: process.env.API_SECRET,
-    },
-    CORS: {
-      origin: "https://find-vacation.herokuapp.com",
-    },
-  },
 };
 
 module.exports = config[env];
